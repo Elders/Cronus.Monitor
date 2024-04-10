0bef7be2dba44d960ec4655e4b2ef56ae282085e
veliodanev <velizardanev11@gmail.com>
2024-04-10 15:52:03 +0300

    fix: names

--- Cronus.Monitor/Cronus.Monitor/App.cs
+++ Cronus.Monitor/Cronus.Monitor/App.cs
@@ -2,6 +2,6 @@
 
 public static class App
 {
-    public const string Name = "Cronus.Monitor";
+    public const string Name = "Cronus.Monitor.Api";
     public const string LogName = "cronus.monitor";
 }
