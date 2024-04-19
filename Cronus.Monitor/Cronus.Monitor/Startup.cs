c70c00643b805ae40db6f6d469c2a575c6177726
mynkow <mynkow@gmail.com>
2024-04-19 10:15:08 +0300

    feat: Updates packages

--- Cronus.Monitor/Cronus.Monitor/Startup.cs
+++ Cronus.Monitor/Cronus.Monitor/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,6 +48,7 @@ public class Startup
         app.ConfigureCors();
 
         app.UseRouting();
+        app.UseSerilogRequestLogging();
 
         app.UseEndpoints(endpoints =>
         {
